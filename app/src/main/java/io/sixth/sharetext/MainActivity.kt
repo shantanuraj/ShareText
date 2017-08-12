ef4aeb19ab4d0ae7d7b7dd57456a4a17e3a0614d
Shantanu Raj <s@sraj.me>
2017-08-13 00:24:15 +0530

    Read sms into pojos

--- app/src/main/java/io/sixth/sharetext/MainActivity.kt
+++ app/src/main/java/io/sixth/sharetext/MainActivity.kt
@@ -8,10 +8,18 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.Toast
+import android.provider.Telephony
+import android.util.Log
+import io.sixth.sharetext.data.Text
+
 
 class MainActivity : AppCompatActivity() {
 
     val APP_REQUEST_READ_SMS: Int = 0x100
+    // Cursor position constants
+    val CURSOR_SENDER   = 0
+    val CURSOR_DATE     = 1
+    val CURSOR_BODY     = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +32,7 @@ class MainActivity : AppCompatActivity() {
             APP_REQUEST_READ_SMS -> {
                 if (grantResults.isNotEmpty() &&
                         grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Got SMS Permission!", Toast.LENGTH_LONG).show()
+                    processSMS()
                 } else {
                     Toast.makeText(this, "No SMS Permission!", Toast.LENGTH_LONG).show()
                 }
@@ -50,11 +58,45 @@ class MainActivity : AppCompatActivity() {
                         APP_REQUEST_READ_SMS)
             }
         } else {
-            Toast.makeText(this, "Has SMS Permission!", Toast.LENGTH_LONG).show()
+            processSMS()
         }
     }
 
     fun onInitClick(view: View) {
         requestSMSPermission()
+    }
+
+    fun processSMS(): Unit {
+        val sms = getAllSMS()
+        sms.forEach { s: Text -> Log.d("NETFLIX", s.toString()) }
+    }
+
+    fun getAllSMS(): ArrayList<Text> {
+        val sms = ArrayList<Text>()
+        val cursor = contentResolver.query(Telephony.Sms.CONTENT_URI,
+                arrayOf(Telephony.Sms.Inbox.ADDRESS,
+                        Telephony.Sms.Inbox.DATE,
+                        Telephony.Sms.Inbox.BODY),
+                null,
+                null,
+                Telephony.Sms.Inbox.DEFAULT_SORT_ORDER)
+
+        val smsCount = cursor!!.count
+
+        if (cursor.moveToFirst()) {
+            for (i in 0..smsCount - 1) {
+                sms.add(Text(
+                        cursor.getString(CURSOR_SENDER),
+                        cursor.getLong(CURSOR_DATE),
+                        cursor.getString(CURSOR_BODY)
+                ))
+                cursor.moveToNext()
+            }
+        } else {
+            Log.d("NETFLIX", "Read fail")
+        }
+
+        cursor.close()
+        return sms
     }
 }
---
+++ app/src/main/java/io/sixth/sharetext/data/Text.kt
@@ -0,0 +1,6 @@
+package io.sixth.sharetext.data
+
+/**
+ * Created by eve on 13/08/17.
+ */
+data class Text(val sender: String, val date: Long, val message: String)
