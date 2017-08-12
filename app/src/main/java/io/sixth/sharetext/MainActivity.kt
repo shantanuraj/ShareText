758671d06cdf28d216b8786e350350991779d207
Shantanu Raj <s@sraj.me>
2017-08-13 01:09:45 +0530

    More cleanup

--- app/src/main/java/io/sixth/sharetext/MainActivity.kt
+++ app/src/main/java/io/sixth/sharetext/MainActivity.kt
@@ -2,7 +2,7 @@ package io.sixth.sharetext
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.widget.Button
 import io.sixth.sharetext.main.SmsReader
 import io.sixth.sharetext.main.SmsReaderImpl
 
@@ -10,11 +10,14 @@ import io.sixth.sharetext.main.SmsReaderImpl
 class MainActivity : AppCompatActivity() {
 
     lateinit var impl: SmsReader
+    lateinit var initButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         impl = SmsReaderImpl(this)
         setContentView(R.layout.activity_main)
+        initButton = findViewById(R.id.init_button)
+        initButton.setOnClickListener { startTextServer() }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
@@ -22,9 +25,9 @@ class MainActivity : AppCompatActivity() {
         impl.requestSMSPermissionResult(requestCode, permissions, grantResults)
     }
 
-    fun onInitClick(view: View) {
+    fun startTextServer() {
         if (impl.hasSMSPermission()) {
-            impl.onSMSPermissionGrant()
+            impl.startTextServer()
         } else {
             impl.requestSMSPermission()
         }
